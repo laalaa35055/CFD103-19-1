1c8973e6393133ef86b7f1b68de52ddf0f21618f
thievesshare <laalaa35055@gmail.com>
2021-10-09 22:16:17 +0800

    v.2.3.5

--- js/config.js
+++ js/config.js
@@ -12,3 +12,31 @@ function S_H(a){
         change.innerHTML = "▼";
     } 
 }
+
+
+function movie_info(a){
+    n = a.substr(5)  
+    console.log(n);
+    var txt = document.getElementById("info_txt"+n);
+    var change = document.getElementById("movie_change"+n);
+    if(txt.style.display === "none" || txt.style.display === ""){
+        txt.style.display = "block";
+        change.innerHTML = "▲";
+    }
+    else{
+        txt.style.display = "none";
+        change.innerHTML = "▼";
+    } 
+}
+
+function show_hide(){
+    var hide_info =  document.getElementById("hide_info");
+    try{    
+        var hide = document.getElementsByClassName("hide movie_info");
+        for(var i =0;i<3;i++){
+            hide[i].className = "movie_info";
+        }
+    }catch{
+        hide_info.innerHTML = "已經到底了！！";
+    }
+}
